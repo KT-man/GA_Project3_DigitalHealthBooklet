0618360f96c68e87b5140de08e58c6b270a803ae
JibbyKT <89538905+JibbyKT@users.noreply.github.com>
2022-07-14 14:33:07 +0800

    Backend template. Need to. change routes and everything

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/db");
+const users = require("./router/users");
+
+const app = express();
+app.use(cors());
+connectDB(process.env.MONGODB_URI);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", users);
+// Find out what this line does
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT);
