b00c1ab1bd8b892580073d727a5f5506b6eea4e0
myriambunting <myriam.bunting@gmail.com>
2022-07-15 21:35:34 +0800

    styled splash page components added

--- frontend/digi-health-book/src/App.js
+++ frontend/digi-health-book/src/App.js
@@ -1,10 +1,12 @@
 import SplashPage from "./components/SplashPage";
 import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <SplashPage />
+      <Signup />
     </div>
   );
 }
---
+++ frontend/digi-health-book/src/components/SplashText.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+function SplashText() {
+  return (
+    <div class="container">
+      <div>
+        <h2 id="what">What can we do with digiBook?</h2>
+        <p>
+          <h3>Log Your Child's Growth</h3>
+          <br />
+          The digi Book Health App will help to log your childs growth data like
+          height and weight over time. Your child will be mapped onto a graph to
+          see where they fit in weight and height percentiles.
+          <h3>Keep Up With Appointments</h3>
+          <br />
+          Keep track of upcoming appointments by storing the time, date and
+          location for each child. You can even opt to add the Doctor's name if
+          your child has multiple doctors or to remember a favourite specialist.
+          View them at any time online and update or add new appoinments.
+        </p>
+      </div>
+      <div>
+        <h2 id="why">Why go online?</h2>
+        <p>
+          Every parent who has a child in Singapore recieves a Health booklet to
+          keep track of the childs growth, vaccinations and health specifics
+          related to their child. All information is stored within these pages.{" "}
+          <br />
+          Dragging it to appointments with you to be updated can be a hassle or
+          is easily forgotten.
+          <br />
+          With a the digi Health Booklet, there is no need to worry about
+          forgetting it at home as you will have it with you. Just log in to
+          view, change or update your child's information or appointment
+          information. <br />
+          Can't remember your child's next appointment? No worries! It's all
+          here for you when you need it.
+        </p>
+      </div>
+      <div>
+        <h2 id="how">How does it work? </h2>
+        <p>
+          <h3>Register</h3>
+          All you need to register is enter your name and password.
+          <h3>Add Your Child's Information</h3>
+          Once registered, you can create a profile for each of your children.
+          Use your current health bookelet as a guide for entering new logs
+          <br />
+          <h3>Making Appointments</h3>
+          Add new appointments for your child's vaccinations and doctor visits
+          All the information about upcoming appointments will be displayed on
+          the cards.
+        </p>
+      </div>
+      <div>
+        <h2 id="contact"> Who are we? </h2>
+        <p>
+          By now you must be thinking.. What a great idea! Who came up with this
+          life hack and how can I get in touch! Send us a message to get in
+          touch with
+          <a href="https://github.com/KT-man/GA_Project3_DigitalHealthBooklet">
+            the developers
+          </a>
+          from GA today or contribute taking the digi Health Booklet App to the
+          next level!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SplashText;
