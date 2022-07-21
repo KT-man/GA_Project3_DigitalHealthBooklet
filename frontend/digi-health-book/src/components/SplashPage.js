7390c20e4ea969c8daa23dd5338fc7cd63f24310
myriambunting <myriam.bunting@gmail.com>
2022-07-21 10:42:08 +0800

    splash

--- frontend/digi-health-book/src/components/SplashPage.js
+++ frontend/digi-health-book/src/components/SplashPage.js
@@ -7,6 +7,7 @@ function SplashPage() {
       <header class="px-5 py-5 bg-aquamarine">
         <img class=" w-40" src="../2.png" alt="digiHealth logo"></img>
         <h1 class="text-slate-600 p-static">digi Health Book SG</h1>
+
         <div class=" max-w-sm pt-4">
           <div>
             <a
@@ -26,13 +27,13 @@ function SplashPage() {
         </div>
       </header>
       <div class="w-full h-2 bg-plumish"></div>
-      <div className="image">
+      <p className="image">
         <img
           class=" w-max-lg flex h-150 "
           src="https://img.freepik.com/free-vector/pediatrician-doctor-woman-examining-baby-boy_3446-535.jpg?t=st=1657858868~exp=1657859468~hmac=f2f9e8770eb5abb6673c6cbcaa4d0025b31785423b3b3275804257fbd824428e"
           alt="pediatrician and baby"
         ></img>
-      </div>
+      </p>
       <div className="nav">
         <ul>
           <li class="px-5 py-1 text-md text-white bg-plumish font-semibold border border-plumish/75 hover:text-white hover:bg-plumish hover:border-transparent">
